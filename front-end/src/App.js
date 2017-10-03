f7638db02f1a18378fc2e7c287727a8954372f7b
learn <learn@learn09.local>
2017-10-03 14:35:27 -0700

    added map page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,13 +3,17 @@ import {
   Grid,
   PageHeader
 } from 'react-bootstrap'
+import Map from './pages/map'
 
 class App extends Component {
   render() {
     return (
       <Grid>
         <PageHeader>
+		<div>
           <h1>THE SAN DIEGO CHALLENGE (tm)</h1>
+		  <Map />
+		</div>
         </PageHeader>
       </Grid>
     );
