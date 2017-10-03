efbed327d6d4d174f3dd4437031e3c02984945db
Darren Littlejohn <darrenlittlejohn@Darrens-MacBook-Pro.local>
2017-10-03 15:34:10 -0700

    Second round of changes.

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Activities from './pages/Activities'
 
-ReactDOM.render(<Activities />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
