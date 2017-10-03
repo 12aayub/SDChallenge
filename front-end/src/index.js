3ca225bec4e08855522905526ca5961cdbb5ac3b
Priyanka Arunkumar <Priyanka@Priyankas-MacBook-Pro.local>
2017-10-03 15:32:47 -0700

    added Activity Modal

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Activities from './pages/Activities'
 
-ReactDOM.render(<Activities />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
