3d7b2c72fc60c36e6250200ee29fb7eb46faa383
learn <learn@learn09.local>
2017-10-05 13:06:10 -0700

    fixed excess package and outdated component import issue

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,32 +10,35 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      activities: [
-        {
-          id: 1,
-          name: 'Museum',
-          address: '2412 J street',
-          description: 'Visit the Art Museum at Balboa Park. Take a picture of yourself in front of your favorite piece of art.',
-          points: 25
-
-        },
-        {
-          id: 2,
-          name: 'Mission Beach',
-          address: '2131 L street',
-          description: 'Take a walk on the boardwalk. Get a picture of yourself in front of the rollercoaster.',
-          points: 45
-
-        },
-        {
-          id: 3,
-          name: 'Hillcrest Farmer\'s Market',
-          address: '19283 B street',
-          description: 'Visit the Hillcrest Farmer\'s Market on Sunday. Take a pictue in front of your favorite vendor.',
-          points: 44
-
-        }
-      ]
+			activities: [
+				{
+					id: 1,
+					name: 'Museum',
+					address: '2412 J street',
+					description: 'Visit the Art Museum at Balboa Park.',
+					points: 25,
+					latitude: 32.709536,
+					longitude: -117.158021
+				},
+				{
+					id: 2,
+					name: 'Mission Beach',
+					address: '2131 L street',
+					description: 'Take a walk on the boardwalk. Get a picture of yourself in front of the rollercoaster.',
+					points: 45,
+					latitude: 32.735073,
+					longitude: -117.148412
+				},
+				{
+					id: 3,
+					name: 'Hillcrest Farmer\'s Market',
+					address: '19283 B street',
+					description: 'Visit the Hillcrest Farmer\'s Market on Sunday. Take a pictue in front of your favorite vendor.',
+					points: 44,
+					latitude: 32.722752,
+					longitude: -117.168310
+				}
+			]
     }
   }
 
@@ -45,17 +48,10 @@ class App extends Component {
         <PageHeader>
 		<div>
           <h1>THE SAN DIEGO CHALLENGE (tm)</h1>
-<<<<<<< HEAD
-
 		</div>
         </PageHeader>
-				<Map />
-        <ActivityModal/>
-        <Activities />
-=======
-        </PageHeader>
+				<Map activities={this.state.activities}/>
         <Activities activities={this.state.activities}/>
->>>>>>> ffe4e284a69afbe86ed5b51b9001afb23c2b202b
       </Grid>
     );
   }
--- front-end/src/pages/map.js
+++ front-end/src/pages/map.js
@@ -10,57 +10,60 @@ const MyMapComponent = compose(
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px`, padding: `20px`}} />,
     mapElement: <div style={{ height: `100%` }} />,
-		center: { lat: 32.709630, lng: -117.158014 },
+		center: { lat: 32.722752, lng: -117.168310 },
   }),
   withScriptjs,
   withGoogleMap
 )((props) =>
   <GoogleMap
-    defaultZoom={14}
+    defaultZoom={12}
     defaultCenter={props.center}
   >
-    {props.isMarkerShown && <Marker position={{ lat: 32.709630, lng: -117.158014 }} onClick={props.onMarkerClick} />}
-		{props.isMarkerShown && <Marker position={{ lat: 32.715405, lng: -117.164008 }} onClick={props.onMarkerClick} />}
-		<InfoBox
-	       defaultPosition={new google.maps.LatLng(props.center.lat, props.center.lng)}
-	       options={{ closeBoxURL: ``, enableEventPropagation: true }}
-				 show={props.isInfoBoxShown}
-	     >
-	       <div style={{ backgroundColor: `white`, opacity: 0.75, padding: `12px` }}>
-					 <div style={{ fontSize: `16px`, fontColor: `#08233B` }}>
-	           Lets try this infobox
-	         </div>
-	       </div>
-	     </InfoBox>
+		{ props.activities.map((activity) =>{
+		return (
+    	props.isMarkerShown && <Marker position={{ lat: activity.latitude, lng: activity.longitude }} onClick={props.onMarkerClick} />
+		)})}
 	</GoogleMap>
 )
 
 
 class Map extends Component {
-	state = {
-    isMarkerShown: false,
-		isInfoBoxShown: false,
+	constructor(props){
+		super(props)
+		this.state = {
+	    isMarkerShown: false,
+	    showModal:false,
+	    currentActivity: null,
+			activities: this.props.activities
+	  }}
+
+	close() {
+    this.setState({ showModal: false });
   }
 
-  componentDidMount() {
-    this.setState({ isMarkerShown: true })
-  }
+	handleMarkerClick (activity) {
+		this.setState({
+			showModal: true,
+			currentActivity: activity
+		});
+	}
 
-
-
-  handleMarkerClick = () => {
-    this.setState({ isInfoBoxShown: true})
-  }
   render() {
     return (
 			<div>
 					<MyMapComponent
 		        isMarkerShown={this.state.isMarkerShown}
 		        onMarkerClick={this.handleMarkerClick}
+						activities={this.state.activities}
 		      />
 		</div>
     );
   }
+
+	componentDidMount() {
+		this.setState({ isMarkerShown: true })
+	}
+
 }
 
 export default Map;
