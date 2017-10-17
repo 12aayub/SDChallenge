28b39a0f74de5731c7e3b636f8291ff3fe8bac4a
learn <learn@learn09.local>
2017-10-17 15:07:52 -0700

    deployment config

--- back-end/index.js
+++ back-end/index.js
@@ -1,5 +1,5 @@
 const app = require('./app')
-
-app.listen(3000, function () {
- console.log('Todo Server listening on port 3000!');
+const port = process.env.PORT || 3000
+app.listen(port, function () {
+ console.log('Todo Server listening on port ' + port + '!');
 });
