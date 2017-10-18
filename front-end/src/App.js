520cd2f5e5d817c1bb2b5de391a3e8d6d49d7cef
learn <learn@learn09.local>
2017-10-18 13:42:25 -0700

    updated about paragraph

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -115,9 +115,11 @@ export default connect(mapComponentToProps)(
                   {
                     (!this.props.user) &&
                     <div>
-                      <h4 id="about">Welcome to the San Diego Challenge! If you’re new or old to San Diego, we think you’ll find these challenges to be a fun and informative way to explore America’s Finest City. We’ve put together a fantastic list that will bring you and your friends and family right in the middle of what makes San Diego amazing.
-                      To get started, just click around the Activities List until you find the one you want to do. Each activity awards points upon completion, Once you get to the location and complete the activity, click the Complete button. Note: You’ll have to be at the location with your GPS on in order to complete the activity! Once activities are completed, your points will be added up with previous completed activities. Your total score will be displayed on your user profile as well as the Leaderboard.
-                      OK that’s it! Have fun exploring!</h4>
+                      <h4 id="about">Welcome to the San Diego Challenge! Whether you’re new or old to San Diego, we think you’ll find these challenges to be a fun and informative way to explore America’s Finest City. We’ve put together a fantastic list that will bring you, your friends, and family to the heart of what makes San Diego amazing.
+                      <p>To get started, just click around the Challenges List until you find the one you want to do. Once you get to the location and complete the challenge, click the Complete button. Each challenge awards points upon completion. Your total score will be displayed on your user profile as well as the Leaderboard.</p>
+                      <p>Note: You’ll have to be at the location with your location services enabled in order to complete the challenge! </p>
+                      <p>OK that’s it! Have fun exploring!</p>
+                      </h4>
                       <ActivitiesAndMap
                         activities={this.props.allActivities}
                         user={this.props.user}
