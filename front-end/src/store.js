2668dc28f03a9afdf963e379928ed4207703d384
learn <learn@learn09.local>
2017-10-18 11:09:59 -0700

    more styling

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,6 +1,5 @@
 import {createStore, applyMiddleware} from 'redux'
 import combinedReducer from './reducers'
-import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 
-export default createStore(combinedReducer, applyMiddleware(thunk, logger))
+export default createStore(combinedReducer, applyMiddleware(thunk))
