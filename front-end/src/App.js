e0ef8dd4e928672667a6eb88553f88faeebad744
learn <learn@learn09.local>
2017-10-20 15:20:07 -0700

    edited about page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -117,9 +117,9 @@ export default connect(mapComponentToProps)(
                     <div>
                       <div id="about">
                         <h4>Welcome to the San Diego Challenge! Whether you’re new or old to San Diego, we think you’ll find these challenges to be a fun and informative way to explore America’s Finest City. We’ve put together a fantastic list that will bring you, your friends, and family to the heart of what makes San Diego amazing.
-                        <p>To get started, choose a challenge from the Challenge List or Map. Once you get to the location, click the Complete button to complete the challenge. Each challenge awards points upon completion. You can view any of your previous challenges on your user profile page as well as your total score. The leaderboard tracks the top 5 players of SDChallenge, try and get the top score!</p>
+                        <p>To get started, choose a challenge from the Challenge List or Map. Once you get to the location, click the Complete button. Each challenge awards points upon completion. You can view your previous challenges on your user profile page as well as your total score.</p>
                         <p>Note: You’ll have to be at the location with your location services enabled in order to complete the challenge.</p>
-                        <p>Have fun exploring!</p>
+                        <p>Check the leaderboard and try and get the top score! Have fun exploring!</p>
                         </h4>
                       </div>
                       <ActivitiesAndMap
